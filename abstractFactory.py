cc36c98cd21d9c15b460f173a7d10e2f160debda
bahatt13 <Baher_Attia@mentor.com>
2023-05-04 14:23:14 +0200

    Implement Abstract Factory Pattern

--- abstractFactory.py
+++ abstractFactory.py
@@ -7,17 +7,36 @@ class Dog:
 	def __str__(self):
 		return "Dog"
 
+class Cat:
+	"""One of the objects to be returned"""
+
+	def speak(self):
+		return "Meow!"
+
+	def __str__(self):
+		return "Cat"
 
 class DogFactory:
-	"""Concrete Factory"""
+	"""Dog Concrete Factory"""
 
 	def get_pet(self):
-		
-
+		"""Returns a Dog object"""
+		return Dog()
 	
 	def get_food(self):
-		
+		"""Returns a Dog Food object"""
+		return "Dog Food!"
 
+class CatFactory:
+	"""Cat Concrete Factory"""
+
+	def get_pet(self):
+		"""Returns a Cat object"""
+		return Cat()
+	
+	def get_food(self):
+		"""Returns a Cat Food object"""
+		return "Cat Food!"
 
 
 class PetStore:
@@ -26,22 +45,33 @@ class PetStore:
 	def __init__(self, pet_factory=None):
 		""" pet_factory is our Abstract Factory """
 
-
+		self._pet_factory = pet_factory
 
 	def show_pet(self):
-		""" Utility method to display the details of the objects retured by the DogFactory """
+		""" Utility method to display the details of the objects returned by the ConcreteFactory (i.e: DogFactory, CatFactory)"""
 
-
+		pet = self._pet_factory.get_pet()
+		pet_food = self._pet_factory.get_food()
 		
 		print("Our pet is '{}'!".format(pet))
 		print("Our pet says hello by '{}'".format(pet.speak()))
 		print("Its food is '{}'!".format(pet_food))
 
 
-#Create a Concrete Factory
-
+#Create a Dag Concrete Factory
+factory = DogFactory()
 
 #Create a pet store housing our Abstract Factory
-
+shop = PetStore(factory)
 
 #Invoke the utility method to show the details of our pet
+shop.show_pet()
+
+#Create a Cat Concrete Factory
+factory = CatFactory()
+
+#Create a pet store housing our Abstract Factory
+shop = PetStore(factory)
+
+#Invoke the utility method to show the details of our pet
+shop.show_pet()
